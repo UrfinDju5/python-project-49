12e2633585c9bfb4c9a0472e1073c6554be7eb3d
Valdemar1988 <qwertysenator@gmail.com>
2023-08-16 21:14:18 +0300

    fix greeting

--- brain_games/greeting.py
+++ brain_games/greeting.py
@@ -5,7 +5,7 @@ def welcome_user():
     print("Welcome to the Brain Games!")
     global name
     name = prompt.string("May I have your name? ")
-    print(f"hello, {name}!")
+    print(f"Hello, {name}!")
     return name
 
 
