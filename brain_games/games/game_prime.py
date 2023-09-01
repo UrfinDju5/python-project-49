368aedc273520eb8cfbd98dfeb6dcd6cd8467d91
Valdemar1988 <qwertysenator@gmail.com>
2023-09-01 19:39:54 +0300

    fix missing whitespace

--- brain_games/games/game_prime.py
+++ brain_games/games/game_prime.py
@@ -23,9 +23,9 @@ def is_prime(num):
     if num < 2:
         return False
     k = 0
-    for i in range(2, num // 2+1):
+    for i in range(2, num // 2 + 1):
         if (num % i == 0):
-            k = k+1
+            k += 1
     if (k <= 0):
         return True
     else:
